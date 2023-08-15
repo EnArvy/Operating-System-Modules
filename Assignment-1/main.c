7f41ac42805cf7b5dc7f9055a002d20b69e4056e
EnArvy <darklacerta7817@gmail.com>
2023-08-15 20:51:58 +0530

    Probably works for inbuilt cmds

--- Assignment-1/main.c
+++ Assignment-1/main.c
@@ -22,9 +22,10 @@
 // <uchar.h> - UTF-16 and UTF-32 character utilities
 // <wchar.h> - Extended multibyte and wide character utilities
 // <wctype.h>
+#define max_cmd_length 2048
 
 int wordcount(char *command, char *delim){
-	char *pch;//for processing words in sentence
+	char *pch;
 	pch = strtok(command,delim);
 	int words=0;
 	while (pch != NULL) {
@@ -35,62 +36,52 @@ int wordcount(char *command, char *delim){
 }
 
 int main(){
+	char PATH[PATH_MAX],PATH_Temp[PATH_MAX];
+	strcpy(PATH,getenv("PATH"));
+	strcpy(PATH_Temp,PATH);
+	int PATH_size= wordcount(PATH_Temp,":");
+
 	while(1){
 		printf("MTL458>");
 
-		char command[1024];
-		fgets(command,1024,stdin);
-		char commandcopy[1024];
+		char command[max_cmd_length];
+		fgets(command,max_cmd_length,stdin);
+		char commandcopy[max_cmd_length];
 		strcpy(commandcopy,command);
 
 		int words = wordcount(command," \n");
 		if(words==0) continue;
+		
 		//Create args array
 		char *args[words+1];
-		args[words]=NULL;
-		if(words==1){
-			args[0] = strtok(commandcopy," \n");
-		}
-		if(words>1){
-			char *pch = strtok(commandcopy," \n");
-			args[0] = pch;
-			int i=1;
-			while(pch != NULL){
-				pch = strtok(NULL," \n");
-				args[i] = pch;
-				i++;
-			}
+		char *pch = strtok(commandcopy," \n");
+		args[0] = pch;
+		int i=1;
+		while(pch != NULL){
+			pch = strtok(NULL," \n");
+			args[i] = pch;
+			i++;
 		}
 
 		int process = fork();
 		if(process==0){
-
 			int found = 0;
 			if(access(args[0],X_OK)==0){
 				found = 1;
 			}
 			else{
-				char path[PATH_MAX],path2[PATH_MAX];
-				strcpy(path,getenv("PATH"));
-				strcpy(path2,path);
-				int pathsize= wordcount(path,":");
-				char paths[pathsize][PATH_MAX];
-				char *pch = strtok(path2,":");
-				strcpy(paths[0],pch);
-				int i=1;
-				while(i!=pathsize){
-					pch = strtok(NULL,":");
-					strcpy(paths[i],pch);
-					i++;
-				}
-				for(int j=0;j!=pathsize;j++){
-					char temp[(int)strlen(paths[j])+1+strlen(args[0])];
-					strcpy(temp,paths[j]);
-					strcat(strcat(temp,"/"),args[0]);
+				strcpy(PATH_Temp,PATH);
+				char *Pch = strtok(PATH_Temp,":");
+				for(int j=1;j<PATH_size;j++){
+					char temp[PATH_MAX];
+					strcpy(temp,Pch);
+					strcat(temp,"/");
+					strcat(temp,args[0]);
 					if(access(temp,X_OK)==0){
 						found = 1;
 						break;
 					}
+					Pch = strtok(NULL,":");
 				}
 			}
 			
@@ -101,8 +92,11 @@ int main(){
 				printf("Command not found\n");fflush(stdout);
 			}
 		}
-		else{
+		else if(process>0){
 			wait(NULL);
+		}
+		else{
+			printf("Error\n");
 		}
 	}
 	return 0;
