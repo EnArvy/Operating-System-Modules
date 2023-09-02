296a74ec0c50139936da2c00e8b32bf58a76a1ba
EnArvy <darklacerta7817@gmail.com>
2023-09-02 19:24:18 +0530

    CHanges

--- Assignment-2/main.c
+++ Assignment-2/main.c
@@ -1,13 +1,32 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+// Structure to represent a process
+struct Process {
+    char pid[32];                  // Process ID
+    int arrival_time;         // Arrival time in microseconds
+    int burst_time;           // Burst time in microseconds
+    int remaining_time;       // Remaining time in microseconds
+    int turnaround_time;      // Turnaround time in microseconds
+    int waiting_time;         // Waiting time in microseconds
+	int job_time;
+};
 
 int main(int argc, char **argv){
-	printf("%d\n", argc);
-
 	FILE *input = fopen(argv[1],"r");
 	FILE *output = fopen(argv[2],"w");
+	struct Process processes[32768];
 
-	for(int i=0;i<argc;i++)
-		fprintf(output,"%s\n",argv[i]);
+	char process[256];
+	int numberProcesses=0;
+	for(int i=0; fgets(process,sizeof(process),input);i++){
+		strcpy(processes[i].pid,strtok(process," "));
+		processes[i].arrival_time = atoi(strtok(NULL," "));
+		processes[i].job_time = atoi(strtok(NULL," "));
+		numberProcesses = i+1;
+	}
+
 
 	return 0;
 }
