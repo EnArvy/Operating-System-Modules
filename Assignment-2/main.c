a93da0a2ea31a668ce735973237cc5959598781c
EnArvy <darklacerta7817@gmail.com>
2023-09-05 01:48:31 +0530

    main.c fcfs rr done

--- Assignment-2/main.c
+++ Assignment-2/main.c
@@ -9,7 +9,7 @@ struct Process {
     int job_time;           // Burst time in microseconds
     int remaining_time;       // Remaining time in microseconds
     int turnaround_time;      // Turnaround time in microseconds
-    int waiting_time;         // Waiting time in microseconds
+    int response_time;         // Waiting time in microseconds
 };
 
 //Linked list of log struct
@@ -41,12 +41,59 @@ void insertLog(struct log **head,char *pid, int start, int finish){
     }
     temp->next = newNode;
 }
+void combineLogs(struct log*head){
+    struct log *temp = head;
+    while(temp->next != NULL){
+        if(strcmp(temp->pid,temp->next->pid) == 0){
+            temp->finish = temp->next->finish;
+            free(temp->next);
+            temp->next = temp->next->next;
+            continue;
+        }
+        temp = temp->next;
+    }
+}
 void printLog(struct log *head){
     while(head != NULL){
         printf("%s %d %d ",head->pid,head->start,head->finish);
         head = head->next;
     }
     printf("\n");
+}
+
+
+void printAvgTurnaroundResponse(struct Process processes[], int n){
+    int sum = 0;
+    for(int i=0;i<n;i++){
+        sum += processes[i].turnaround_time;
+    }
+    printf("%.2f ",(float)sum/n);
+    sum=0;
+    for(int i=0;i<n;i++){
+        sum += processes[i].response_time;
+    }
+    printf("%.2f\n",(float)sum/n);
+}
+
+
+// Function to simulate the First Come First Serve scheduling algorithm
+struct log* fcfs(struct Process processes[], int n) {
+    int current_time = 0;
+    struct log* head=NULL;
+
+    for (int i = 0; i < n; i++) {
+        if (processes[i].arrival_time > current_time) {
+            current_time = processes[i].arrival_time;
+        }
+
+        processes[i].turnaround_time = current_time + processes[i].job_time - processes[i].arrival_time;
+        processes[i].response_time = current_time - processes[i].arrival_time;
+
+        insertLog(&head,processes[i].pid,current_time,current_time + processes[i].job_time);
+
+        current_time += processes[i].job_time;
+    }
+    return head;
 }
 
 
@@ -63,7 +110,9 @@ struct log* roundRobin(struct Process processes[], int n, int time_slice) {
             int execution_time = (processes[current_process].remaining_time < time_slice)
                                     ? processes[current_process].remaining_time
                                     : time_slice;
-
+            if(processes[current_process].response_time == -1){
+                processes[current_process].response_time = current_time - processes[current_process].arrival_time;
+            }
             current_time += execution_time;
             processes[current_process].remaining_time -= execution_time;
 
@@ -72,23 +121,13 @@ struct log* roundRobin(struct Process processes[], int n, int time_slice) {
             if (processes[current_process].remaining_time == 0) {
                 remaining_processes--;
                 processes[current_process].turnaround_time = current_time - processes[current_process].arrival_time;
-                processes[current_process].waiting_time = processes[current_process].turnaround_time - processes[current_process].job_time;
             }
         }
 
         current_process = (current_process + 1) % n;
     }
 
-    //Combine continuouse execution of same process
-    struct log *temp = head;
-    while(temp->next != NULL){
-        if(strcmp(temp->pid,temp->next->pid) == 0){
-            temp->finish = temp->next->finish;
-            temp->next = temp->next->next;
-            continue;
-        }
-        temp = temp->next;
-    }
+    combineLogs(head);
     return head;
 }
 
@@ -98,6 +137,7 @@ int main(int argc, char **argv){
 	FILE *output = freopen(argv[2],"w",stdout);
 	struct Process processes[32768],clonedProcesses[32768];
 	int TsRR=atoi(argv[3]),TsMLFQ1=atoi(argv[4]),TsMLFQ2=atoi(argv[5]),TsMLFQ3=atoi(argv[6]),BMLFQ=atoi(argv[7]);
+    struct log* head;
 
 	char process[256];
 	int numberProcesses=0;
@@ -106,14 +146,18 @@ int main(int argc, char **argv){
 		processes[i].arrival_time = atoi(strtok(NULL," "));
 		processes[i].job_time = atoi(strtok(NULL," "));
         processes[i].remaining_time = processes[i].job_time;
+        processes[i].response_time = -1;
 		numberProcesses = i+1;
 	}
 
-    struct log* head;
-	// // FCFS();
+    for(int i=0;i<numberProcesses;i++)clonedProcesses[i]=processes[i];
+	head = fcfs(clonedProcesses,numberProcesses);
+    printLog(head);
+    printAvgTurnaroundResponse(clonedProcesses,numberProcesses);
 	for(int i=0;i<numberProcesses;i++)clonedProcesses[i]=processes[i];
 	head = roundRobin(clonedProcesses,numberProcesses,TsRR);
     printLog(head);
+    printAvgTurnaroundResponse(clonedProcesses,numberProcesses);
 	// ShortestJobFirst();
 	// ShortestTimeRemaining();
 	// MLFQ();
